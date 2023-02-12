dd85e06b7e26ad55488ed2df589584c1c9f596ca
Nit89 <nitesh382singh.com>
2023-02-12 21:37:37 +0530

    custom user model,media files and django signals

--- foodOnline_main/foodOnline_main/settings.py
+++ foodOnline_main/foodOnline_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -82,8 +83,11 @@ DATABASES = {
         'HOST':'localhost',
         
         
+        
     }
 }
+
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Password validation
@@ -120,12 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'foodOnline_main/static',
    
 ]
+
+# Media files configuratiom
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
