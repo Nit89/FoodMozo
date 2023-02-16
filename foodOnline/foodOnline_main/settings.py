213e3bbde4a31cf3b712e0f4343369a901d3ff37
Nit89 <nitesh382singh.com>
2023-02-16 21:54:15 +0530

    static file error solved  but collect static left

--- foodOnline/foodOnline_main/settings.py
+++ foodOnline/foodOnline_main/settings.py
@@ -125,10 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+# STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
-    'foodOnline_main/static',
-   
+    BASE_DIR / "static"   
 ]
 
 # Media files configuratiom
