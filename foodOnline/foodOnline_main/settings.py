f3c4f41582632e0047e69a41efb705a508fd1bc6
Nitesh  singh <nitesh382singh@gmail.com>
2023-03-12 00:46:45 +0530

    token verification and email configuration

--- foodOnline/foodOnline_main/settings.py
+++ foodOnline/foodOnline_main/settings.py
@@ -139,3 +139,13 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Email configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'djangofoodonline95@gmail.com'
+EMAIL_HOST_PASSWORD = "anntjayhixsorcyp"
+DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <djangofoodonline95@gmail.com>'
+
+# EMAIL_HOST_PASSWORD = "qwert@898"
+EMAIL_USE_TLS = True
+
