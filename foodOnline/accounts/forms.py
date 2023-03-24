cbd860527f72ee197b7af2aa43cdd235d32df152
Nitesh  singh <nitesh382singh@gmail.com>
2023-03-25 00:17:00 +0530

    vendor profile validation

--- foodOnline/accounts/forms.py
+++ foodOnline/accounts/forms.py
@@ -1,5 +1,9 @@
-from django import forms  
-from .models import User
+from django import forms
+
+from  .validators import allow_only_images_validator  
+from .models import User,UserProfile
+
+
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -18,7 +22,22 @@ class UserForm(forms.ModelForm):
                 "password does not matching"
             )
         
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    
+   
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
         
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
         
         
     
